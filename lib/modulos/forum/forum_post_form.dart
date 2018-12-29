94b78af51200ae54956dbb80cb274e2b3a08d73e
George Rappel <george.concei@hotmail.com>
2018-12-28 22:09:53 -0200

    Adicionada criação de posts no fórum

--- lib/modulos/forum/forum_post_form.dart
+++ lib/modulos/forum/forum_post_form.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:redesign/estilos/tema.dart';
 import 'package:redesign/modulos/forum/forum_post.dart';
 import 'package:redesign/modulos/forum/forum_tema.dart';
 import 'package:redesign/servicos/meu_app.dart';
+import 'package:redesign/widgets/botao_padrao.dart';
 import 'package:redesign/widgets/tela_base.dart';
 
 class ForumPostForm extends StatefulWidget {
@@ -17,14 +21,80 @@ class ForumPostForm extends StatefulWidget {
 class ForumPostFormState extends State<ForumPostForm> {
   ForumTema tema;
 
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool blocked = false;
+
+  ForumPost post = ForumPost();
+
   ForumPostFormState(this.tema);
 
   @override
   Widget build(BuildContext context) {
     return TelaBase(
       title: "Nova Postagem",
-      body: Text("Formulário"),
+      body: Scaffold(
+        key: _scaffoldKey,
+        resizeToAvoidBottomPadding: false,
+        body: Form(
+          key: _formKey,
+          autovalidate: true,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.short_text),
+                  labelText: 'Título',
+                ),
+                validator: (val) => val.isEmpty ? 'Título é obrigatório' : null,
+                inputFormatters: [LengthLimitingTextInputFormatter(50)],
+                onSaved: (val) => post.titulo = val,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.description),
+                  labelText: 'Descrição',
+                ),
+                keyboardType: TextInputType.multiline,
+                maxLines: 4,
+                validator: (val) => val.isEmpty ? 'Descrição é obrigatório' :
+                val.length > 15 ? null : 'Descreva melhor seu problema',
+                inputFormatters: [LengthLimitingTextInputFormatter(500)],
+                onSaved: (val) => post.descricao = val,
+              ),
+              Container(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: BotaoPadrao("Salvar", _submitForm,
+                      Tema.principal.primaryColor, Tema.cinzaClaro)
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  void _submitForm() {
+    if(blocked) return;
+
+    blocked = true;
+    final FormState form = _formKey.currentState;
+
+    if (!form.validate()) {
+      showMessage('Por favor, complete todos os campos.');
+    } else {
+      form.save(); //Executa cada evento "onSaved" dos campos do formulário
+      post.criadoPor = MeuApp.userId();
+      post.temaId = tema.reference.documentID;
+      post.data = DateTime.now();
+      savePost(post);
+    }
+  }
+
+  void showMessage(String message, [MaterialColor color = Colors.red]) {
+    blocked = false;
+    _scaffoldKey.currentState
+        .showSnackBar(SnackBar(backgroundColor: color, content: Text(message)));
   }
 
   savePost(ForumPost post){
