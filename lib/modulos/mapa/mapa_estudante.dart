ace37ed47581bd77f50e132fbda7985eaf3c35cb
George Rappel <george.concei@hotmail.com>
2019-02-12 13:55:32 -0200

    + Mapa estudante - Implementar Image view com coordenadas do toque

--- lib/modulos/mapa/mapa_estudante.dart
+++ lib/modulos/mapa/mapa_estudante.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:photo_view/photo_view.dart';
+import 'package:image_tap_coordinates/image_tap_coordinates.dart';
 
 class MapaEstudante extends StatefulWidget {
   @override
@@ -9,23 +9,24 @@ class MapaEstudante extends StatefulWidget {
 
 class _MapaEstudanteState extends State<MapaEstudante> {
 
+  ImageTapController _controller;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: GestureDetector(
-        child: PhotoView(
-          backgroundDecoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          imageProvider: AssetImage("images/mapa_rede.png"),
-          minScale: PhotoViewComputedScale.contained,
-          initialScale: PhotoViewComputedScale.contained * 2.8,
-          maxScale: PhotoViewComputedScale.contained * 4,
-        ),
-        onTapDown: (tapDownDetails) => print(tapDownDetails.globalPosition),
+      child: ImageTapCoordinates(
+        AssetImage("images/mapa_rede.png"),
+        backgroundColor: Colors.white,
+        controller: _controller,
+        initScale: 0.133,
+        minScale: 0.056,
+        maxScale: 0.8,
+        tapCallback: (Offset tapCoordinates){
+          print(tapCoordinates);
+        },
       ),
     );
-  }
 
+  }
 }
